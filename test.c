b6088c3ba6b9beefb421de82454ef16a4f1d027c
EstebanZepeda <esteban.zepedac@userena.cl>
2024-05-15 10:36:24 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,25 @@
-//hi
+#include <stdio.h>
+
+typedef struct EnteroLargo{
+ char signo;
+ unsigned short tamañoDigitos;
+ struct *lista; 
+
+}EnteroLargo;
+
+
+
+
+typedef struct lista{
+
+struct *anterior
+char Digito;
+struct *siguiente
+}lista;
+
+
+void crearLista(){
+  lista = char* malloc(sizeof(char));
+  lista->anterior = NULL;
+  lista->siguiente = NULL;
+}
